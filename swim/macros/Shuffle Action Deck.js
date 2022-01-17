b54723c28ee72de41fad04f9f5b744ac46249964
SalieriC <salieric@t-online.de>
2022-01-17 07:51:45 +0100

    fix #36

--- swim/macros/Shuffle Action Deck.js
+++ swim/macros/Shuffle Action Deck.js
@@ -1,4 +1,8 @@
-const table = game.tables.entities.find(t => t.name === "Action Cards");
+/*
+ * Deprecated in favour of the systems native reshuffle button in the combat tracker.
+ */
+
+const table = game.tables.getName("Action Cards");
 table.reset();
 ui.notifications.info("Action Deck shuffled.");
 AudioHelper.play({ src: `systems/swade/assets/card-flip.wav` }, true);
