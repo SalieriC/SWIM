05189956f03f34b26b8c4f3364d3d0cd13782853
SalieriC <salieric@t-online.de>
2022-04-01 17:07:59 +0200

    revert the revert

--- swim/scripts/swim_modules/unstun.js
+++ swim/scripts/swim_modules/unstun.js
@@ -1,6 +1,6 @@
 /*******************************************
  * Unstun macro for SWADE
- * version v.4.0.2
+ * version v.4.0.3
  * Made and maintained by SalieriC#8263 using original Code from Shteff.
  ******************************************/
 
@@ -77,8 +77,6 @@ export async function unstun_script() {
                 await succ.apply_status(token, 'distracted', false)
                 await swim.wait(delay)
                 await succ.apply_status(token, 'stunned', false)
-                await swim.wait(delay)
-                await succ.apply_status(token, 'prone', false)
                 if (unshakeSFX) { AudioHelper.play({ src: `${unshakeSFX}` }, true); }
             } else {
                 chatData += ` and remains Stunned.`;
