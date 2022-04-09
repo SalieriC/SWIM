b50f3f02aeada86322ec2ad7164d49cfe534e36a
SalieriC <salieric@t-online.de>
2022-04-09 19:52:33 +0200

    combat setup

--- swim/scripts/init.js
+++ swim/scripts/init.js
@@ -85,6 +85,41 @@ Hooks.on(`ready`, () => {
     warpgate.event.watch("SWIM.deleteActor", gm_relay.gmDeleteActor, swim.is_first_gm)
 });
 
+// Combat setup playlist handling
+Hooks.on("preUpdateCombat", async (combat, update, options, userId) => {
+    if (game.settings.get("swim", "combatPlaylistManagement") === true && combat.round === 0 && update.round === 1) {
+        if (swim.is_first_gm() === false) { return }
+        const combatPlaylistName = game.settings.get("swim", "combatPlaylist")
+        const combatPlaylist = game.playlists.getName(combatPlaylistName)
+        const protectedFolder = game.settings.get("swim", "noStopFolder")
+        for (let playlist of game.playlists.playing) {
+            if (playlist.folder?.name.toLowerCase() != protectedFolder.toLowerCase()) {
+                playlist.setFlag('swim', 'resumeAfterCombat', true)
+                playlist.stopAll()
+            }
+        }
+        if (combatPlaylist) {
+            combatPlaylist.playAll()
+        }
+    }
+})
+
+Hooks.on("deleteCombat", async (combat, options, userId) => {
+    if (game.settings.get("swim", "combatPlaylistManagement") === true) {
+        if (swim.is_first_gm() === false) { return }
+        for (let playlist of game.playlists) {
+            if (playlist.data.flags?.swim?.resumeAfterCombat === true) {
+                await playlist.playAll()
+                playlist.unsetFlag('swim', 'resumeAfterCombat')
+            }
+        }
+        const combatPlaylist = game.playlists.find(p => p.name.toLowerCase() === "combat") //needs setting
+        if (combatPlaylist && combatPlaylist.data.playing === true) {
+            await combatPlaylist.stopAll()
+        }
+    }
+})
+
 //BR2 Hooks
 Hooks.on(`BRSW-Unshake`, async (message, actor) => {
     const { shakenSFX, deathSFX, unshakeSFX, soakSFX } = await swim.get_actor_sfx(actor)
