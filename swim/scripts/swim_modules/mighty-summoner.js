b395413a4cc240e8065d81eec420ed842ca03d07
SalieriC <salieric@t-online.de>
2022-06-13 07:35:04 +0200

    versioning

--- swim/scripts/swim_modules/mighty-summoner.js
+++ swim/scripts/swim_modules/mighty-summoner.js
@@ -16,7 +16,7 @@
  * also play a visual effect. SFX and VFX are configured
  * in the module settings of SWIM.
  * 
- * v. 1.2.2
+ * v. 1.2.3
  * By SalieriC
  ******************************************************/
  function generate_id (length = 16) {
