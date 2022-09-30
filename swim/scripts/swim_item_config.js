10113e2d08c0fd2a6963dafd90cb0db29a7e684f
Emanuel May <loofou@ret-world.de>
2022-09-30 21:04:16 +0200

    Creating AE UI from a button in config window

--- swim/scripts/swim_item_config.js
+++ swim/scripts/swim_item_config.js
@@ -21,16 +21,72 @@ class ItemConfigForm extends FormApplication {
     }
 
     activateListeners(html) {
-        return super.activateListeners(html);
+        super.activateListeners(html);
+        html = html[0];
+
+        html.querySelectorAll("[data-action]").forEach(button => {
+            button.addEventListener("click", this._onButtonClick.bind(this));
+        });
     }
 
     getData() {
-        return {obj: this.object};
+        if (this.object.type === 'gear') {
+            return {
+                options: {
+                    ammo_title: {
+                        isSectionTitle: true,
+                        value: "Ammo Management"
+                    },
+                    isAmmo: {
+                        isBoolean: true,
+                        id: 'is-ammo',
+                        label: 'Is Ammunition',
+                        value: false
+                    },
+                    ammoAE: {
+                        isAE: true,
+                        label: 'Set Ammo Active Effect',
+                        id: 'ammo-active-effect',
+                        value: ''
+                    }
+                }
+            };
+        }
+        return {};
+    }
+
+    async _onButtonClick(event) {
+        event.preventDefault();
+        const action = event.currentTarget.dataset.action;
+        if (this[action])
+            await this[action](event);
+    }
+
+    async _setActiveEffect(event) {
+        let defaults;
+        const oldValue = event.currentTarget.dataset.value;
+
+        if (typeof oldValue === 'string' && oldValue.trim().length > 0) {
+            defaults = JSON.parse(oldValue);
+        } else {
+            defaults = {
+                label: 'Ammo Effect',
+                icon: '/icons/svg/mystery-man-black.svg'
+            }
+        }
+
+        const effect = await CONFIG.ActiveEffect.documentClass.create(defaults, {
+            renderSheet: true,
+            parent: this.object
+        });
+        console.log(effect);
+
+        const effectString = JSON.stringify(effect);
     }
 
     async _updateObject(_, formData) {
         //Add flags updating here
 
-        this.render();
+        this.render(true);
     }
 }
