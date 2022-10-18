397764968422071bcd23d544d8e92287563e4b33
SalieriC <salieric@t-online.de>
2022-10-18 10:59:45 +0200

    sfx constants update

--- swim/scripts/hooks/brsw_hooks.js
+++ swim/scripts/hooks/brsw_hooks.js
@@ -2,7 +2,7 @@ export async function brsw_hooks() {
     //BR2 Hooks
     Hooks.on(`BRSW-Unshake`, async (message, actor) => {
         if (game.settings.get("swim", "br2Support") === true) {
-            const { shakenSFX, deathSFX, unshakeSFX, soakSFX } = await swim.get_actor_sfx(actor)
+            const { shakenSFX, deathSFX, unshakeSFX, stunnedSFX, soakSFX, fatiguedSFX, looseFatigueSFX } = await swim.get_actor_sfx(actor)
             if (unshakeSFX) {
                 await swim.play_sfx(unshakeSFX)
             }
@@ -13,7 +13,7 @@ export async function brsw_hooks() {
     });*/
     Hooks.on("BRSW-AfterApplyDamage", async (token, final_wounds, final_shaken, incapacitated, initial_wounds, initial_shaken, soaked) => {
         if (game.settings.get("swim", "br2Support") === true) {
-            const { shakenSFX, deathSFX, unshakeSFX, soakSFX } = await swim.get_actor_sfx(token.actor)
+            const { shakenSFX, deathSFX, unshakeSFX, stunnedSFX, soakSFX, fatiguedSFX, looseFatigueSFX } = await swim.get_actor_sfx(actor)
             const volume = Number(game.settings.get("swim", "defaultVolume"))
             if (soaked >= 1) {
                 await swim.play_sfx(soakSFX, volume)
