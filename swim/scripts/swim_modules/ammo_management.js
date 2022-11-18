848c22fe1548330f54c1874f9b103e6e11b09df2
SalieriC <salieric@t-online.de>
2022-11-18 17:06:40 +0100

    versioning

--- swim/scripts/swim_modules/ammo_management.js
+++ swim/scripts/swim_modules/ammo_management.js
@@ -1,6 +1,6 @@
 /*******************************************
  * Ammo Management (Enhanced Version v2)
- * version 6.0.0
+ * version 6.0.4
  * By SalieriC#8263 & Loofou#7406. (old Dialogue Framework: Kekilla#7036)
  *
  * Makes heavy use of SFX set up on the weapon.
