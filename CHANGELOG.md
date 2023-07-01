567106721049482a8eaf7a6e11f034ceeed43c22
SalieriC <salieric@t-online.de>
2023-07-01 10:12:35 +0200

    restore toggle functionality

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -32,11 +32,13 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Fixed some other bugs when dismissing summoned tokens.
 - Fixed deprecation warning resulting from using `effect.flags.core.statusId`.
 - Fixed a bug that opened the token vision macro if the macro was used to create the effect.
+- Fixed a bug in the Mark Dead macro that prevented the removal of Incapacitation. It now toggles on/off again.
 ### Removed
 - Some translation strings no longer needed.
 - Default value for campfire as leaving that in was an oversight. SWIM does not come with a texture for campfires yet, I hiighly recommend the animated one from JB2A though.
 - Old compendium .db-files no longer used in v11.
 - Compendium Folders is no longer a dependancy.
+- Removed Health Estimate support with a heavy heart as changes in the module prevent SWIM from supporting it.
 ### Known issues
 - Mirror Self only properly works if the summoning token is linked.
 
--- swim/scripts/swim_modules/mark_dead.js
+++ swim/scripts/swim_modules/mark_dead.js
@@ -3,7 +3,7 @@
  * This Macro works best with Health Estimate installed.
  * It will mark the selected tokens as dead.
  * If the selected token is dead, it will be marked as alive instead.
- * v. 4.0.4 by SalieriC.
+ * v. 4.0.5 by SalieriC.
  *******************************************/
 export async function mark_dead_script() {
     const { _, __, ___, token } = await swim.get_macro_variables()
@@ -19,7 +19,10 @@ export async function mark_dead_script() {
 
     async function main() {
         for (let e of canvas.tokens.controlled) {
-            await succ.toggle_status(e, 'incapacitated', true, true)
+            const isInc = await succ.check_status(e, 'incapacitated')
+            const apply = !isInc //invert the result to remove if applied and vice versa.
+            console.log(e, isInc, apply)
+            await succ.toggle_status(e, 'incapacitated', apply, true)
         }
         ui.notifications.info(game.i18n.localize("SWIM.notification.markDeadAlive"));
         AudioHelper.play({ src: `${deathSFX}` }, true);
