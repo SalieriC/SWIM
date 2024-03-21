ba0efe2e4313309d390972e5aababf5c6cabee0c
SalieriC <salieric@t-online.de>
2024-03-21 22:48:19 +0100

    missed a function

--- swim/scripts/swim_modules/mark_dead.js
+++ swim/scripts/swim_modules/mark_dead.js
@@ -20,13 +20,13 @@ export async function mark_dead_script() {
     async function main() {
         for (let e of canvas.tokens.controlled) {
             if (e.type === 'npc' || e.type === 'vehicle') {
-                const isInc = await succ.check_status(e, 'defeated')
+                const isInc = await game.succ.hasCondition('defeated', e)
                 const apply = !isInc //invert the result to remove if applied and vice versa.
                 //await succ.toggle_status(e, 'defeated', apply, true)
                 if (apply) { await game.succ.addCondition('defeated', e, {forceOverlay: true}) }
                 else if (!apply) { await game.succ.removeCondition('defeated', e) }
             } else if (e.type === 'character') {
-                const isInc = await succ.check_status(e, 'incapacitated')
+                const isInc = await game.succ.hasCondition('incapacitated', e)
                 const apply = !isInc //invert the result to remove if applied and vice versa.
                 //await succ.toggle_status(e, 'incapacitated', apply, true)
                 if (apply) { await game.succ.addCondition('incapacitated', e, {forceOverlay: true}) }
